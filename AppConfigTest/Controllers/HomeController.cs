eed41df39acd7832e2fcc4ef20c8b637c3ea8278
sleepergoose <sleepergoose@gmail.com>
2021-02-03 23:07:44 +0200

    Prepared project for lesson Descriptor helper class

---
+++ AppConfigTest/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppConfigTest.Models;
+
+namespace AppConfigTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ICityRepository repository;
+        public HomeController(ICityRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index()
+        {
+            return View(repository.Cities);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.AddCity(city);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(city);
+            }
+        }
+    }
+}
---
+++ AppConfigTest/Models/CityRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppConfigTest.Models
+{
+    public interface ICityRepository
+    {
+        IEnumerable<City> Cities { get; }
+        void AddCity(City city);
+
+    }
+
+    public class CityRepository : ICityRepository
+    {
+        private List<City> cities = new List<City>()
+        {
+            new City { Name = "London", Country = "England", Population = 8_258_369 },
+            new City { Name = "New York", Country = "USA", Population = 9_558_369 },
+            new City { Name = "Moscow", Country = "Russia", Population = 7_158_369 },
+            new City { Name = "Shefild", Country = "England", Population = 858_369 },
+            new City { Name = "Manchester", Country = "England", Population = 1_458_369 }
+        };
+        public IEnumerable<City> Cities => cities;
+
+        public void AddCity(City city)
+        {
+            cities.Add(city);
+        }
+    }
+}
